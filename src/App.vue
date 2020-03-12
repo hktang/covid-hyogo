2cf8307fed7263069f03eabf8868c1c29daaf65b
Xiaojun Deng <xdeng@apn-gcr.org>
2020-03-12 15:19:28 +0900

    Hello vue.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Index pageTitle="COVID-19 Hyogo"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Index from './components/Index.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Index
   }
 }
 </script>
