3aef9e111322c0857d2cd0271c73c693aea78bee
Xiaojun Deng <hiukuan.tang@gmail.com>
2020-03-15 00:24:23 +0900

    Add graphic.

--- src/App.vue
+++ src/App.vue
@@ -3,35 +3,21 @@
     <h1>COVID-19 Hyogo</h1>
     <ChartByDateContainer />
     <MapByCity />
-    <div class="footer">
-      <h2>Primary data source</h2>
-      <p>
-        Data from
-        <a href="https://web.pref.hyogo.lg.jp/kk03/corona_hasseijyokyo.html">
-          新型コロナウイルスに感染した患者の発生状況</a
-        >, Hyogo Prefectural Government
-      </p>
-      <p>
-        Available as
-        <a
-          href="https://docs.google.com/spreadsheets/d/e/2PACX-1vTM9vP7fGowD3SKSGDGpIWBpbGHLclWW2TmbGyejLM5e3h9CwWvz9sP8a7I0C-F98bL6L-grr7bF6X5/pubhtml"
-        >
-          Google Spreadsheet</a
-        >
-      </p>
-    </div>
+    <Footer />
   </div>
 </template>
 
 <script>
 import ChartByDateContainer from "./components/ChartByDateContainer.vue";
 import MapByCity from "./components/MapByCity.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
   name: "App",
   components: {
     ChartByDateContainer,
-    MapByCity
+    MapByCity,
+    Footer
   }
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="footer">
+    <h2>Wait, did you wash your hands?</h2>
+    <img
+      alt="Often missed"
+      class="hands"
+      src="../assets/handwashing-missed.png"
+    />
+    <h2>References</h2>
+
+    <p>
+      Data from
+      <a href="https://web.pref.hyogo.lg.jp/kk03/corona_hasseijyokyo.html">
+        新型コロナウイルスに感染した患者の発生状況</a
+      >, Hyogo Prefectural Government
+    </p>
+    <p>
+      Data available as
+      <a
+        href="https://docs.google.com/spreadsheets/d/e/2PACX-1vTM9vP7fGowD3SKSGDGpIWBpbGHLclWW2TmbGyejLM5e3h9CwWvz9sP8a7I0C-F98bL6L-grr7bF6X5/pubhtml"
+      >
+        Google Spreadsheet</a
+      >
+    </p>
+    <p>
+      Graphic by Taylor LJ via
+      <a
+        href="https://flowingdata.com/2020/02/18/most-often-missed-areas-while-washing-hands/"
+        >Flowing Data</a
+      >
+    </p>
+  </div>
+</template>
+<style scoped>
+.hands {
+  width: 40vw;
+}
+</style>
