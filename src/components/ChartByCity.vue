f93974503e7267daee8658f2e00fdcdb9ee6326e
Xiaojun Deng <hiukuan.tang@gmail.com>
2020-03-14 22:15:44 +0900

    Okay, stamen is best.

--- src/components/ChartByCity.vue
+++ src/components/ChartByCity.vue
@@ -43,8 +43,8 @@ export default {
     return {
       zoom: 9,
       center: [34.833439, 134.993893],
-      url: 'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png',
-      attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+      url: 'https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.png',
+      attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       circles: [],
       options: {
         scrollWheelZoom:false, 
