c8cebced06efd81439fb49b5db0885a209e2d337
Xiaojun Deng <hiukuan.tang@gmail.com>
2020-03-15 15:04:10 +0900

    Add Sentry.io.

--- src/main.js
+++ src/main.js
@@ -2,9 +2,16 @@ import Vue from "vue";
 import App from "./App.vue";
 import "leaflet/dist/leaflet.css";
 import "vue-loading-overlay/dist/vue-loading.css";
+import * as Sentry from "@sentry/browser";
+import * as Integrations from "@sentry/integrations";
 
 Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App)
 }).$mount("#app");
+
+Sentry.init({
+  dsn: "https://534d3fcaa0da4806a747945b80051083@sentry.io/4771975",
+  integrations: [new Integrations.Vue({ Vue, attachProps: true })]
+});
