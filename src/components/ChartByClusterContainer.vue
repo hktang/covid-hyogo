75c2b98b3dd0073cf3c22cc03948aae8385866f0
Xiaojun Deng <hiukuan.tang@gmail.com>
2020-03-17 00:41:44 +0900

    Add chart to show clusters.

--- src/components/ChartByClusterContainer.vue
+++ src/components/ChartByClusterContainer.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="container">
     <h2>Clusters</h2>
-    <loading :active.sync="loading"></loading>
     <chart-by-cluster
       v-if="loaded"
       class="chart"
@@ -21,8 +20,8 @@ export default {
   data: () => ({
     loaded: false,
     chartdata: null,
-    xMap: ["", "Kindergarten", "Livehouse", "Hospital", ""],
-    yMap: ["", "January", "February", "March", "April", "May", "June", "July"]
+    xLabels: [],
+    yLabels: []
   }),
   async mounted() {
     this.getData();
@@ -31,37 +30,83 @@ export default {
     getData: function() {
       axios
         .get(
-          "https://spreadsheets.google.com/feeds/cells/1B0aXcDc2IOkKRcWqoQzVsswoJ-rd5hXp8DYgT9KyqDw/4/public/basic?alt=json"
+          "https://spreadsheets.google.com/feeds/cells/" +
+            process.env.VUE_APP_GOOGLE_SPREADSHEET_ID +
+            "/6/public/basic?alt=json"
         )
         .then(response => {
           const responseData = response.data;
-          console.log(responseData);
+
+          const headerRow = responseData.feed.entry.filter(entry => {
+            return (
+              entry["title"]["$t"].substring(1, 2) === "1" &&
+              entry["title"]["$t"].length == 2
+            );
+          });
+
+          const dateColumn = responseData.feed.entry.filter(entry => {
+            return entry["title"]["$t"].substring(0, 1) === "B";
+          });
+
+          const cases = responseData.feed.entry.filter(entry => {
+            return entry["content"]["$t"].trim() === "〇";
+          });
+
+          this.header = headerRow.map(item => item["content"]["$t"]);
+          this.xLabels = headerRow.map(item => item["content"]["$t"]);
+          this.xLabels.splice(0, 2);
+          this.yLabels = dateColumn.map(item => item["content"]["$t"]);
+          this.yLabels.splice(0, 1);
+
+          const caseData = [];
+
+          for (let i = 0; i < cases.length; i++) {
+            const label = cases[i]["title"]["$t"];
+            const rowNum = label.substring(1);
+            const colAlpha = label.substring(0, 1);
+
+            const theDate = responseData.feed.entry.filter(entry => {
+              return entry["title"]["$t"] === "B" + rowNum;
+            });
+
+            const theLabel = responseData.feed.entry.filter(entry => {
+              return entry["title"]["$t"] === colAlpha + "1";
+            });
+
+            caseData.push(
+              this.mapDataPoint(
+                theLabel[0]["content"]["$t"],
+                theDate[0]["content"]["$t"]
+              )
+            );
+          }
+
           this.chartdata = {
             datasets: [
               {
-                label: "My First dataset",
-                data: [
-                  this.mapDataPoint("Kindergarten", "January"),
-                  this.mapDataPoint("Livehouse", "February"),
-                  this.mapDataPoint("Hospital", "February"),
-                  this.mapDataPoint("Kindergarten", "March"),
-                  this.mapDataPoint("Hospital", "March")
-                ],
+                data: caseData,
                 fill: false,
                 showLine: false,
-                pointRadius: 10,
-                borderColor: "#acacac",
-                backgroundColor: "#cacaca"
+                opacity: 0.5,
+                pointRadius: 4,
+                borderWidth: 2,
+                borderColor: "#809399",
+                backgroundColor: "rgba(255, 255, 255, 0)"
               }
             ]
           };
           this.options = {
             responsive: true,
+            maintainAspectRatio: true,
+            aspectRatio: 0.2,
             title: {
               display: false
             },
             legend: {
               display: false
+            },
+            tooltips: {
+              enabled: false
             },
             scales: {
               xAxes: [
@@ -73,24 +118,28 @@ export default {
                   },
                   ticks: {
                     min: 0,
-                    max: this.xMap.length - 1,
+                    max: this.xLabels.length - 1,
                     callback: value => {
-                      return this.xMap[value];
+                      return this.xLabels[value];
                     }
                   }
                 }
               ],
               yAxes: [
                 {
+                  type: "linear",
                   scaleLabel: {
                     display: false
                   },
+                  gridLines: {
+                    display: false
+                  },
                   ticks: {
-                    reverse: true,
+                    reverse: false,
                     min: 0,
-                    max: this.yMap.length - 1,
-                    callback: value => {
-                      return this.yMap[value];
+                    max: this.yLabels.length + 1,
+                    callback: i => {
+                      return this.yLabels[i];
                     }
                   }
                 }
@@ -98,7 +147,6 @@ export default {
             }
           };
           this.loaded = true;
-          this.loading = false;
         })
         .catch(error => {
           console.log(error);
@@ -107,8 +155,8 @@ export default {
     // Courtesy of https://stackoverflow.com/questions/43090102/chartjs-mapping-non-numeric-y-and-x
     mapDataPoint: function(xValue, yValue) {
       return {
-        x: this.xMap.indexOf(xValue),
-        y: this.yMap.indexOf(yValue)
+        x: this.xLabels.indexOf(xValue),
+        y: this.yLabels.indexOf(yValue)
       };
     }
   }
@@ -117,14 +165,22 @@ export default {
 
 <style scoped>
 .container {
+  margin: 0 auto;
+  padding-bottom: 40px;
 }
 
 .chart {
 }
 
 @media only screen and (max-width: 800px) {
+  .container {
+    width: 95vw;
+  }
 }
 
 @media only screen and (min-width: 800px) {
+  .container {
+    width: 50vw;
+  }
 }
 </style>
