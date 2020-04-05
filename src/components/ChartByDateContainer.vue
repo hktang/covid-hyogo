2c51b3b394b1f6d76921a9119b1bd9915e62311e
hktang <hiukuan.tang@gmail.com>
2020-04-05 19:19:23 +0900

    Update datetime format.

--- src/components/ChartByDateContainer.vue
+++ src/components/ChartByDateContainer.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <p class="muted">
       {{ $t("overview.lastUpdated") }}
-      {{ new Date(lastUpdated) | moment("YYYY-M-D H:m:s") }} (JST)
+      {{ new Date(lastUpdated) | moment("YYYY-M-D H:mm:ss") }} (JST)
     </p>
     <dl>
       <i18n path="overview.confirmedCases" tag="dt">
