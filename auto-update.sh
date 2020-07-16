55e730c5bcde90edcd896625fe2bbf1aea992114
X <hiukuan.tang@gmail.com>
2020-07-16 22:01:27 +0900

    Update data Thu Jul 16 13:01:27 UTC 2020.

--- auto-update.sh
+++ auto-update.sh
@@ -8,14 +8,17 @@ else
 
   echo $(date -u)  "Something changed." >> ~/covid-hyogo/updater.log 
 
+  cd ~/covid-hyogo
+
   # Update Google Spreadsheet
-  python3 ./src/updater/updateGoogleSheet.py >> ~/covid-hyogo/updater.log
+  python3 ~/covid-hyogo/src/updater/updateGoogleSheet.py >> ~/covid-hyogo/updater.log
 
   # Update local json data
   yarn scrape >> ~/covid-hyogo/updater.log
 
   # Commit and push
   git checkout master
+  git pull origin master
   git add . >> ~/covid-hyogo/updater.log
   git commit -m "Update data $(date -u)." >> ~/covid-hyogo/updater.log
   git push origin HEAD  => ~/covid-hyogo/updater.log
