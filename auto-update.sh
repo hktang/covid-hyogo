b4f6e5d0ddd21d2e97d1f205048b3b1b3a29b7fe
X <hiukuan.tang@gmail.com>
2020-07-18 13:06:18 +0900

    Update data Sat Jul 18 04:06:18 UTC 2020.

--- auto-update.sh
+++ auto-update.sh
@@ -2,25 +2,25 @@
 
 if [[ -z $(urlwatch) ]]; then
 
-  echo $(date -u) "Nothing changed." >> ~/covid-hyogo/updater.log
+  echo $(date -u) "Nothing changed." >> ~/covid-hyogo/updater.log 2>&1
 
 else
 
-  echo $(date -u)  "Something changed." >> ~/covid-hyogo/updater.log 
+  echo $(date -u)  "Something changed." >> ~/covid-hyogo/updater.log 2>&1
 
   cd ~/covid-hyogo
 
   # Update Google Spreadsheet
-  python3 ~/covid-hyogo/src/updater/updateGoogleSheet.py >> ~/covid-hyogo/updater.log
+  python3 ~/covid-hyogo/src/updater/updateGoogleSheet.py >> ~/covid-hyogo/updater.log 2>&1
 
   # Update local json data
-  yarn scrape >> ~/covid-hyogo/updater.log
+  yarn scrape ~/covid-hyogo/updater.log 2>&1
 
   # Commit and push
   git checkout master
   git pull origin master
-  git add . >> ~/covid-hyogo/updater.log
-  git commit -m "Update data $(date -u)." >> ~/covid-hyogo/updater.log
-  git push origin HEAD  => ~/covid-hyogo/updater.log
+  git add . >> ~/covid-hyogo/updater.log 2>&1 
+  git commit -m "Update data $(date -u)." >> ~/covid-hyogo/updater.log 2>&1
+  git push origin HEAD >> ~/covid-hyogo/updater.log 2>&1
 
 fi
