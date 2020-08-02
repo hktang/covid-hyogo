8c1d8a04b45aec2a61dcfa848926cbbc8874c115
X <hiukuan.tang@gmail.com>
2020-08-02 09:53:50 +0900

    Disable auto update.

--- auto-update.sh
+++ auto-update.sh
@@ -8,19 +8,19 @@ else
 
   echo $(date -u)  "Something changed." >> ~/covid-hyogo/updater.log 2>&1
 
-  cd ~/covid-hyogo
+  # cd ~/covid-hyogo
 
   # Update Google Spreadsheet
   # python3 ~/covid-hyogo/src/updater/updateGoogleSheet.py >> ~/covid-hyogo/updater.log 2>&1
 
   # Update local json data
-  yarn scrape >> ~/covid-hyogo/updater.log 2>&1
+  # yarn scrape >> ~/covid-hyogo/updater.log 2>&1
 
   # Commit and push
-  git checkout master
-  git pull origin master
-  git add . >> ~/covid-hyogo/updater.log 2>&1 
-  git commit -m "Update data $(date -u)." >> ~/covid-hyogo/updater.log 2>&1
-  git push origin HEAD >> ~/covid-hyogo/updater.log 2>&1
+  # git checkout master
+  # git pull origin master
+  # git add . >> ~/covid-hyogo/updater.log 2>&1 
+  # git commit -m "Update data $(date -u)." >> ~/covid-hyogo/updater.log 2>&1
+  # git push origin HEAD >> ~/covid-hyogo/updater.log 2>&1
 
 fi
